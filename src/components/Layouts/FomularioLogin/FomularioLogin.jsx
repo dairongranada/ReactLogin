834e883db2aca41e9c50b59f98930e043d88e591
Dairon Granada <93607018+dairongranada@users.noreply.github.com>
2022-08-26 07:06:38 -0500

    Se agrego la Función Del Login -DG

--- src/components/Layouts/FomularioLogin/FomularioLogin.jsx
+++ src/components/Layouts/FomularioLogin/FomularioLogin.jsx
@@ -5,38 +5,63 @@ import { useState,useEffect } from 'react'
 
 export const FomularioLogin = () => {
 
+    const [formLogin, setFormLogin] = useState([])
+    const[userName,setUserName] = useState([])
+
+
+    const [passNameLogin,setPassNameLogin ]= useState("")
+    //Obetener Valor Del Input UserName
+    const valueNameLogin = (e) => {setPassNameLogin(e.target.value)}
+
+
+    const [passwordLogin,setPasswordLogin ]= useState("")
+    //Obetener Valor Del Input Password
+    const valuePassLogin = (e) => {setPasswordLogin(e.target.value)}
+
+
+
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch('https://backend-edw.herokuapp.com/usuarios');
             const data = await res.json();
-            console.log(data);
+            setFormLogin(data)
         }
         fetchData();
-    }, [])
+    }, [])  
+
+
+
+const handleclick = ()=>{
+    formLogin.find(function(e) {
+        if (e[1] === passNameLogin && e[3] === passwordLogin ) {
+            console.log('INICIO DE SESION EXITOSO!')
+        }else{console.log('REGISTRESE!')}
+    })
+}
 
 
 
     return (
         <div  className="containerlogin">
             <div className="login">
-                <form>
                 <h1>INICIO DE SESION</h1>
                     <div className="grupForm">
-                        <input id="inputLoginNameUsuario"  required type="text"/>
+                        <input onChange={valueNameLogin}  required type="text"/>
                         <label>Nombre Usuario</label>
                     </div>
                     <div className="grupForm">
-                        <input required type="password"/>
+                        <input required  onChange={valuePassLogin} type="password" />
                         <label>Contraseña </label>
                     </div>
-                <button type='submit'className="button">Iniciar sesión</button>
+                <button type='submit'className="button"  onClick={handleclick} >Iniciar sesión</button>
                 <div className= "ContenedoresDePregunta">
                     <Link to="#"> ¿A olvidado Su contraseña? </Link>
                     <Link to="/Registro"> ¿No tienes una una Cuenta? </Link>
                 </div>
-                </form>
             </div>
         </div> 
     )
 }
+
+
 
